aae058a04978ac65a595f77c7c6bbf7e59831365
Yogesh Yadav <yogeshyadavsocial@gmail.com>
2026-08-12 15:46:01 +0530

    chore: add dependencies

---
+++ dependencies.py
@@ -0,0 +1,57 @@
+from fastapi.params import Depends
+
+from infrastructure.database import get_db
+from infrastructure.redis_db import get_redis_client
+
+from repositories.booking_repository import BookingRepository
+from repositories.room_repository import RoomRepository
+from repositories.user_repository import UserRepository
+
+from services.auth_service import AuthService
+from services.booking_service import BookingService
+from services.room_service import RoomService
+
+"""
+dependencies for getting repository
+"""
+def get_room_repository(
+    db = Depends(get_db)
+):
+    return RoomRepository(db)
+
+def get_booking_repository(
+    db = Depends(get_db)
+):
+    return BookingRepository(db)
+
+def get_user_repository(
+    db = Depends(get_db)
+):
+    return UserRepository(db)
+
+
+"""
+dependencies for getting services
+"""
+def get_auth_service(
+    user_repository = Depends(get_user_repository),
+    redis = Depends(get_redis_client)
+):
+    return AuthService(
+        user_repository=user_repository,
+        redis=redis
+    )
+
+def get_booking_service(
+    booking_repository = Depends(get_booking_repository)
+):
+    return BookingService(
+        booking_repository = booking_repository
+    )
+
+def get_room_service(
+    room_repository = Depends(get_room_repository)
+):
+    return RoomService(
+        room_repository = room_repository
+    )
