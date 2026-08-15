33c3f16afe7601014a543ac26de12e4723f514e7
Yogesh Yadav <yogeshyadavsocial@gmail.com>
2026-08-15 17:39:56 +0530

    chore: dependencies and helpers

--- constants.py
+++ constants.py
@@ -1,3 +1,4 @@
+import os
 from enum import Enum
 
 class EmailVerificationStatus(str, Enum):
@@ -12,3 +13,6 @@ SESSION_EXPIRY_SECONDS = 60 * 60 * 24
 VERIFICATION_EXPIRY_SECONDS = 60 * 30
 RESET_EXPIRY_SECONDS = 60 * 30
 
+BOOKING_SERVICE_BASE_URL = os.getenv("BOOKING_SERVICE_BASE_URL", "localhost:8000")
+
+EMAIL_SENDING_ID = os.getenv("EMAIL_SENDING_ID")
--- dependencies.py
+++ dependencies.py
@@ -1,6 +1,7 @@
 from fastapi import Depends, Cookie
 
 from infrastructure.database import get_db
+from infrastructure.email_service import EmailService
 from infrastructure.redis_db import get_redis_client
 
 from repositories.booking_repository import BookingRepository
@@ -33,13 +34,18 @@ def get_user_repository(
 """
 dependencies for getting services
 """
+def get_email_service():
+    return EmailService()
+
 def get_auth_service(
     user_repository = Depends(get_user_repository),
-    redis = Depends(get_redis_client)
+    redis = Depends(get_redis_client),
+    email_service = Depends(get_email_service)
 ):
     return AuthService(
         user_repository=user_repository,
-        redis=redis
+        redis=redis,
+        email_service = email_service
     )
 
 def get_booking_service(
@@ -61,7 +67,7 @@ def get_room_service(
 dependency to get user_if from session_id obtained from cookie
 """
 async def get_current_user(
-    session_id : str | None = Cookie(default = None),
+    session_id : str | None = Cookie(default = None, alias='session_id'),
     auth_service : AuthService = Depends(get_auth_service)
 ):
     return await auth_service.get_current_user(session_id)
