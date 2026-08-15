1dba2071f9e97abe302468f2fa3e00c902c2b21e
Yogesh Yadav <yogeshyadavsocial@gmail.com>
2026-08-15 15:32:32 +0530

    chore: add generators

--- utils.py
+++ utils.py
@@ -1,6 +1,16 @@
 from argon2 import PasswordHasher
+import secrets
+import uuid
+
 password_hasher = PasswordHasher()
 
 def hash_password(password : str) ->str :
     return password_hasher.hash(password)
 
+
+def generate_random_token() -> str:
+    return secrets.token_urlsafe(32)
+
+
+def generate_uuid() -> str:
+    return str(uuid.uuid4())
