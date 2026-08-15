5e57c9c5beb55027e3ed0d0e8a7623f6e96ba707
Yogesh Yadav <yogeshyadavsocial@gmail.com>
2026-08-15 15:34:24 +0530

    chore: new custom exceptions

--- exceptions.py
+++ exceptions.py
@@ -1,2 +1,11 @@
 class UserAlreadyExists(Exception):
     pass
+
+class AuthenticationError(Exception):
+    pass
+
+class EmailVerificationError(Exception):
+    pass
+
+class AlreadyVerifiedError(Exception):
+    pass
