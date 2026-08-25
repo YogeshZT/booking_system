57c32e27906a11ec65e8554cff538b5a26416b1a
Yogesh Yadav <yogeshyadavsocial@gmail.com>
2026-08-25 10:57:11 +0530

    feat: create booking

--- api/bookings.py
+++ api/bookings.py
@@ -1,6 +1,8 @@
 from fastapi  import APIRouter, Depends
 
 from dependencies import get_booking_service, get_current_user
+from responses.booking_messages import *
+from responses.common import SuccessResponse
 from schemas.booking import CreateBookingRequest
 from services.booking_service import BookingService
 
@@ -9,35 +11,43 @@ router = APIRouter(
 )
 
 @router.post("/")
-def create_booking(
+async def create_booking(
     payload : CreateBookingRequest,
     user_id = Depends(get_current_user),
     booking_service : BookingService = Depends(get_booking_service)
 ):
-    return booking_service.create_booking(payload = payload, user_id = user_id)
+    booking_id = await booking_service.create_booking(payload = payload, user_id = user_id)
+
+    return SuccessResponse(
+        status = True,
+        message = BOOKING_CREATED_MESSAGE,
+        data = {
+            "booking_id": booking_id
+        }
+    )
 
 
 @router.get("/me")
-def get_user_bookings(
+async def get_user_bookings(
     user_id = Depends(get_current_user),
     booking_service : BookingService = Depends(get_booking_service)
 ):
-    return booking_service.get_user_bookings(user_id)
+    return await booking_service.get_user_bookings(user_id)
 
 
 @router.get("/{booking_id}")
-def get_booking_details(
+async def get_booking_details(
     booking_id :str,
     user_id = Depends(get_current_user),
     booking_service : BookingService = Depends(get_booking_service)
 ):
-    return booking_service.get_booking_details(booking_id = booking_id, user_id = user_id)
+    return await booking_service.get_booking_details(booking_id = booking_id, user_id = user_id)
 
 
 @router.delete("/{booking_id}")
-def delete_booking(
+async def delete_booking(
     booking_id : str,
     user_id = Depends(get_current_user),
     booking_service : BookingService = Depends(get_booking_service)
 ):
-    return booking_service.delete_booking(booking_id = booking_id, user_id = user_id)
+    return await booking_service.delete_booking(booking_id = booking_id, user_id = user_id)
