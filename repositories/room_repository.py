98b9a6752e068dc5df322788cd4a3f8dcb674be7
Yogesh Yadav <yogeshyadavformal@gmail.com>
2026-08-26 12:13:21 +0530

    chore: room repository functions

--- repositories/room_repository.py
+++ repositories/room_repository.py
@@ -1,4 +1,6 @@
 from sqlalchemy import select
+
+from constants import RoomStatus
 from models import Room
 from utils import generate_uuid
 
@@ -7,11 +9,18 @@ class RoomRepository:
     def __init__(self, db):
         self.db = db
 
+
+    async def commit_and_refresh(self, room : Room):
+        await self.db.commit()
+        await self.db.refresh(room)
+
+
     async def get_room_details(self, room_id : str) -> Room | None:
         room = await self.db.scalar(
             select(Room).where(Room.id == room_id)
         )
         return room
+
 
     async def create_room(self, room_name : str, user_id : str) -> Room | None:
         room_id = generate_uuid()
@@ -23,10 +32,10 @@ class RoomRepository:
         )
 
         self.db.add(room)
-        await self.db.commit()
-        await self.db.refresh(room)
+        await self.commit_and_refresh(room)
 
         return room
+
 
     async def edit_room_details(self, room_id : str, new_room_name : str, user_id : str) -> Room | None:
         room = await self.db.scalar(
@@ -37,7 +46,14 @@ class RoomRepository:
 
         room.name = new_room_name
         room.updated_by = user_id
-        await self.db.commit()
-        await self.db.refresh(room)
+        await self.commit_and_refresh(room)
 
         return room
+
+
+    async def get_active_rooms(self) -> list[Room]:
+        result = await self.db.execute(
+            select(Room).where(Room.status == RoomStatus.ACTIVE)
+        )
+
+        return result.scalars().all()
