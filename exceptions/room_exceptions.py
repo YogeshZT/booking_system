7c5c7d32ab17bf3bc34a282e6d736ff10995fdf4
Yogesh Yadav <yogeshyadavformal@gmail.com>
2026-08-26 12:11:11 +0530

    chore: custom room exceptions

--- exceptions/room_exceptions.py
+++ exceptions/room_exceptions.py
@@ -8,9 +8,33 @@ class CannotCreateRoomError(AppException):
             status_code = 400
         )
 
+
 class CannotEditRoomError(AppException):
     def __init__(self):
         super().__init__(
             message = "Cannot edit room details",
+            status_code = 400
+        )
+
+
+class CannotFetchRoomDetailsError(AppException):
+    def __init__(self):
+        super().__init__(
+            message = "Cannot fetch room details",
+            status_code = 400
+        )
+
+
+class CannotBlockRoomError(AppException):
+    def __init__(self):
+        super().__init__(
+            message = "Cannot block room",
+            status_code = 400
+        )
+
+class CannotUnblockRoomError(AppException):
+    def __init__(self):
+        super().__init__(
+            message = "Cannot unblock room",
             status_code = 400
         )
